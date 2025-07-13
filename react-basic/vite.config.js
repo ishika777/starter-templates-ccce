43aa578a183629a059b7dac8ed55810ff41ff677
Ishika Knsal <ishikakasal05@gmail.com>
2025-07-14 00:41:11 +0530

    modified vite.config.js for docker

--- react-basic/vite.config.js
+++ react-basic/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: '0.0.0.0',
-        port: 3000, // Match this with your Docker EXPOSE and PortBindings
+        port: 5000, // Match this with your Docker EXPOSE and PortBindings
         strictPort: true, // Ensures it doesn't randomly use another port
     },
 });
